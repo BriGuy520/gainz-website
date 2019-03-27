278a60ad9f1eef55d04079da36d22fd4efa20e46
Brian Falasz <brianfalasz@gmail.com>
2019-03-27 14:26:54 -0500

    added link to survey to the 2nd slide

--- server/client/src/components/slider/Cover2.js
+++ server/client/src/components/slider/Cover2.js
@@ -3,6 +3,18 @@ import React from 'react';
 const Cover2 = () => {
   return (
     <div className="cover2">
+      <div className="cover2-copy"> 
+        <h1>What do you Want?</h1>
+        <p>
+          Get started by filling out a short survey to get a better 
+          gauge of what your fitness goals are. 
+        </p>
+        <button className="ui button red survey-button">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSf0K6Cd3_EZdQcZTnOR6kUowWdVk2-hl34FqBiMpy4gw7JDtw/viewform">
+            Take Survey
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
