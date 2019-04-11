7cf1e02aa8c9f81fcafd2284dc925c02430c8e91
Brian Falasz <brianfalasz@gmail.com>
2019-04-11 15:23:34 -0500

    made a few small chnages to the styling of the contact section

--- server/client/src/components/Contact.js
+++ server/client/src/components/Contact.js
@@ -3,16 +3,18 @@ import Footer from './Footer';
 
 const Contact = () => {
   return (
-    <div id="contact" className="contact-section">
-      <h1>Are You Ready to Make Gains?</h1>
-      <p>Contact John personally through email and get started changing your life today.</p>
-      <div className="contact-box">
-        <i class="envelope outline icon"></i>
-        <p>email:
-          <a href="mailto:johnnygainesfit@gmail.com">johnnygainesfit@gmail.com</a>
-        </p>
+    <div className="contact-container">
+      <div id="contact" className="contact-section">
+        <h1>Are You Ready to Make Gains?</h1>
+        <p>Contact John personally through email and get started changing your life today.</p>
+        <div className="contact-box">
+          <i class="envelope outline icon"></i>
+          <p>email:
+            <a href="mailto:johnnygainesfit@gmail.com">johnnygainesfit@gmail.com</a>
+          </p>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }
