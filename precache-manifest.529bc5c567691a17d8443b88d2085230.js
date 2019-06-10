3dcdcce8b31a8adbfd3e36bc3bdeb6d17fdfb745
Brian Falasz <bfalasz@yahoo.com>
2019-06-10 15:44:21 -0500

    Updates

---
+++ precache-manifest.529bc5c567691a17d8443b88d2085230.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b6c2e2fe938cb2ddb368acc97c5064c",
+    "url": "/gainz-website/static/media/IMG_1165.8b6c2e2f.png"
+  },
+  {
+    "revision": "e4ce09f9f9d2028e381c",
+    "url": "/gainz-website/static/css/main.6b9b405d.chunk.css"
+  },
+  {
+    "revision": "65534f3aeda50c3e20bc",
+    "url": "/gainz-website/static/js/runtime~main.1383fca6.js"
+  },
+  {
+    "revision": "c06863185bccca55f0fe",
+    "url": "/gainz-website/static/js/2.35bd3f85.chunk.js"
+  },
+  {
+    "revision": "7de1d5b0a668062938eafba9ab2c1a22",
+    "url": "/gainz-website/static/media/gainz-logo.7de1d5b0.png"
+  },
+  {
+    "revision": "e4ce09f9f9d2028e381c",
+    "url": "/gainz-website/static/js/main.dc9128a9.chunk.js"
+  },
+  {
+    "revision": "9847c9e114cb24f863a04d13c7e6166d",
+    "url": "/gainz-website/static/media/IMG_0261.9847c9e1.jpg"
+  },
+  {
+    "revision": "450e006f9aec4db9857384fa491a74f5",
+    "url": "/gainz-website/static/media/IMG_0189.450e006f.jpg"
+  },
+  {
+    "revision": "31659d89e9a03c1575ee750d71de42e6",
+    "url": "/gainz-website/static/media/IMG_0187.31659d89.jpg"
+  },
+  {
+    "revision": "316f3f1f3c3e2593f104ec7cb5f39d3b",
+    "url": "/gainz-website/static/media/gym-background.316f3f1f.jpg"
+  },
+  {
+    "revision": "d7d0b4c0bc95d5f7f14584caafe61338",
+    "url": "/gainz-website/index.html"
+  }
+];
--- server/middleware/login.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if(!req.user){
-    return res.redirect("/login");
-  }
-
-  next();
-};
-
